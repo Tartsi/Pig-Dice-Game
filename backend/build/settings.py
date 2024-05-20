46b655f6e039f3c9b5e4c0ef9f65dd3f87dab613
Tartsi <tarvo.lilja@hotmail.fi>
2024-05-20 19:14:59 +0300

    More conf, needs fix!

--- backend/build/settings.py
+++ backend/build/settings.py
@@ -51,7 +51,7 @@ ROOT_URLCONF = 'build.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['frontend/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
