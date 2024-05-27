df7afcbddc1883cd475695ca01a22ed13a16bf62
Tartsi <tarvo.lilja@hotmail.fi>
2024-05-27 22:18:05 +0300

    (backend) Add game-session + reset functionality

--- backend/game/urls.py
+++ backend/game/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, pig_view
+from .views import index, pig_view, reset
 
 urlpatterns = [
     path('', index, name='index'),
-    path('pig', pig_view, name='pig_view')
+    path('pig', pig_view, name='pig_view'),
+    path('reset/', reset, name='reset')
 ]
--- backend/game/views.py
+++ backend/game/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import render
-
+from django.http import JsonResponse
+from django.shortcuts import redirect, render
 from .game_logic import PigGame
-
-# Create your views here.
 
 
 def index(request):
@@ -20,13 +18,38 @@ def index(request):
 
 def pig_view(request):
     """
-    Renders the 'pig.html' template.
+    Renders the 'pig.html' template and initializes a new game session.
 
     Parameters:
     - request: The HTTP request object.
 
     Returns:
-    - A rendered HTML response.
+    - A rendered HTML response along.
 
     """
+
+    if 'game' not in request.session:
+        request.session['game'] = PigGame().to_dict()
+        print(request.session['game'])
+
     return render(request, 'pig.html')
+
+
+def reset(request):
+    """
+    Resets the game session and redirects the user to index-page.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        JsonResponse: A JSON response indicating the status of the reset operation.
+        - If the game session exists and is deleted, response will have a 'status' of 'success'.
+        - If the game session does not exist, the response will have a 'status' of 'error'.
+    """
+
+    if 'game' in request.session:
+        del request.session['game']
+        return JsonResponse({'status': 'success'})
+
+    return JsonResponse({'status': 'error'})
