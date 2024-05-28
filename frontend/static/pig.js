ee1f926940e152fd3eb404e1ab6fdc56a424dd8f
Tarvo Lilja <tarvo.lilja@hotmail.fi>
2024-05-28 21:45:28 +0300

    (frontend) Add JS-functionality to hold + buttons + CSS for winner

--- frontend/static/pig.js
+++ frontend/static/pig.js
@@ -19,14 +19,36 @@ const rollDice = () => {
 
             if (roll === 1) {
                 currentScore = 0;
-                player0NameEl.classList.toggle('active');
-                player1NameEl.classList.toggle('active');
-                currentScore0El.innerText = 'Current Score: 0';
-                currentScore1El.innerText = 'Current Score: 0';
+                changeTurn();
             }
 
             playerCurrentScoreEl.innerText = `Current Score: ${currentScore}`;
             document.getElementById('dice-pic').src = `/static/assets/dice-${roll}.png`;
+        }
+    });
+};
+
+const hold = () => {
+    fetch('/hold')
+    .then(response => response.json())
+    .then(data => {
+        if (data.status === 'success') {
+
+            const playerTotalScoreEl = document.getElementById(`score--${data.current_turn}`);
+            const totalScore = data.scores[data.current_turn];
+
+            if (totalScore >= 100) {
+                const winnerEl = document.getElementById(`player${data.current_turn}`);
+                winnerEl.appendChild(document.createElement('br'));
+                winnerEl.appendChild(document.createTextNode('Winner!'));
+                winnerEl.classList.add('winner');
+                player0NameEl.classList.remove('active');
+                player1NameEl.classList.remove('active');
+                document.getElementById('dice-pic').src = '/static/assets/dice-1.png';
+            } else {
+                playerTotalScoreEl.innerText = `${totalScore}`;
+                changeTurn();
+            }
         }
     });
 };
@@ -39,4 +61,11 @@ const resetSession = () => {
             window.location.href = '/';
         }
     });
+};
+
+const changeTurn = () => {
+    player0NameEl.classList.toggle('active');
+    player1NameEl.classList.toggle('active');
+    currentScore0El.innerText = 'Current Score: 0';
+    currentScore1El.innerText = 'Current Score: 0';
 };
