e37c0755f545ddcd745f2afdbed6668e64795577
Tartsi <tarvo.lilja@hotmail.fi>
2024-05-29 23:37:49 +0300

    (frontend) Add async restart, button + functionality

--- frontend/static/pig.js
+++ frontend/static/pig.js
@@ -2,6 +2,8 @@
 
 const player0NameEl = document.getElementById('player0');
 const player1NameEl = document.getElementById('player1');
+const totalScore0El = document.getElementById('score--0');
+const totalScore1El = document.getElementById('score--1');
 const currentScore0El = document.getElementById('current-score--0');
 const currentScore1El = document.getElementById('current-score--1');
 
@@ -39,9 +41,8 @@ const hold = () => {
 
             if (totalScore >= 100) {
                 const winnerEl = document.getElementById(`player${data.current_turn}`);
-                winnerEl.appendChild(document.createElement('br'));
-                winnerEl.appendChild(document.createTextNode('Winner!'));
                 winnerEl.classList.add('winner');
+                winnerEl.appendChild(document.createTextNode('🎉 Winner! 🎉'));
                 player0NameEl.classList.remove('active');
                 player1NameEl.classList.remove('active');
                 document.getElementById('dice-pic').src = '/static/assets/dice-1.png';
@@ -68,4 +69,34 @@ const changeTurn = () => {
     player1NameEl.classList.toggle('active');
     currentScore0El.innerText = 'Current Score: 0';
     currentScore1El.innerText = 'Current Score: 0';
+};
+
+const restartGame = () => {
+    fetch('/restart')
+    .then(response => response.json())
+    .then(data => {
+        if (data.status === 'success') {
+
+            totalScore0El.innerText = '0';
+            totalScore1El.innerText = '0';
+            currentScore0El.innerText = 'Current Score: 0';
+            currentScore1El.innerText = 'Current Score: 0';
+
+            player0NameEl.classList.add('active');
+            player1NameEl.classList.remove('active');
+            player0NameEl.classList.remove('winner');
+            player1NameEl.classList.remove('winner');
+
+            if (player0NameEl.lastChild.textContent === '🎉 Winner! 🎉') {
+                player0NameEl.removeChild(player0NameEl.lastChild);
+            }
+
+            if (player1NameEl.lastChild.textContent === '🎉 Winner! 🎉') {
+                player1NameEl.removeChild(player1NameEl.lastChild);
+            }
+
+            document.getElementById('dice-pic').src = '/static/assets/dice-1.png';
+
+        }
+    });
 };
